84d3efd67707af3eac36ecc45e03511c8c0664b8
T8840 <673683677@qq.com>
2023-04-28 16:07:58 +0800

    add .

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: FC = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Discord Bot Inviter</title>
+        <meta name="description" content="Invite your Discord Bot" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Discord Bot Inviter
+        </h1>
+
+        <div className={styles.buttonWrapper}>
+          <a href="https://discord.com/api/oauth2/authorize?client_id=1101333903865806979&permissions=0&scope=bot%20applications.commands" target="_blank" rel="noopener noreferrer" className={styles.button}>
+            Inviter
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Powered by Branche AI</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
